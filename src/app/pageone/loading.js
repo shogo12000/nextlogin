f9efa23318dd564961dbacb2b9de1e35abcd8389
sidney <shogo12000@hotmail.com>
2025-03-11 02:13:54 -0700

    first commit

---
+++ src/app/pageone/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-10 h-10 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  
--- src/app/pageone/page.js
+++ src/app/pageone/page.js
@@ -2,19 +2,18 @@
 'use client';
 
 import LogoutButton from "../components/logoutButton/logoutButton";
-import { Suspense } from 'react'
+
 
 
 
 
 const pageone = () => {
-  return (
-    <Suspense fallback={<p>Loading feed...</p>}>
+  return ( 
       <div className="w-full h-screen flex flex-col justify-center items-center">
         <h1>Página authenthicado</h1>
         <LogoutButton />
       </div>
-    </Suspense>
+ 
   );
 };
 
