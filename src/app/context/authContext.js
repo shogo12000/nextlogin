8a1f13eefd5bc50ed22575eff5255035d971a2b2
sidney <shogo12000@hotmail.com>
2025-03-11 01:13:36 -0700

    first commit

--- src/app/context/authContext.js
+++ src/app/context/authContext.js
@@ -1,16 +1,17 @@
 'use client';
 import { createContext, useEffect, useState } from "react";
-
+import { useRouter } from "next/router";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) =>{
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const router = useRouter();
     
     const checkAuth = async()=>{
         const response = await (await fetch("/api/auth/checkToken")).json();
         setIsAuthenticated(response.token);
-        console.log(response.token)
-        
+ 
+        router.push('/login');
     }
     
     useEffect(()=>{
